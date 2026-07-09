c06f139225361905b3dac7179d733bea320feecc
Tim Fehler <126073245+TimFehler@users.noreply.github.com>
2026-07-09 11:08:44 +0200

    Added vimtex plugin

--- modules/languages/tex.nix
+++ modules/languages/tex.nix
@@ -1,4 +1,36 @@
-{
+{pkgs, ...}: {
+  vim.startPlugins = [
+    pkgs.vimPlugins.vimtex
+  ];
+
+  vim.keymaps = [
+    {
+      key = "<localleader>lk";
+      mode = "n";
+      action = "<cmd>VimtexCompile<CR>";
+      desc = "Compile LaTeX";
+    }
+
+    {
+      key = "<localleader>lv";
+      mode = "n";
+      action = "<cmd>VimtexView<CR>";
+      desc = "View LaTeX PDF";
+    }
+    
+    {
+      key = "<localleader>lt";
+      mode = "n";
+      action = "<cmd>VimtexTocToggle<CR>";
+      desc = "View LaTeX TOC";
+    }
+  ];
+
+  vim.globals = {
+    vimtex_view_method = "zathura";
+    vimtex_compiler_method = "latexmk";
+  };
+
   vim.languages.tex = {
     enable = true;
 
